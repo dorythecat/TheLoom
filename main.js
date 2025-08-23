16422f5e5b51851c10b34743a6215038a8ace6d8
Dory <pemegomachado@gmail.com>
2025-08-23 22:35:09 +0200

    Added cube edges

--- main.js
+++ main.js
@@ -7,17 +7,31 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Create a box
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+// Create the box's edges and add them to the scene
+const edges = new THREE.EdgesGeometry(geometry);
+const line = new THREE.LineSegments(
+    edges,
+    new THREE.LineBasicMaterial({color: '#000000'})
+);
+scene.add(line);
 
 camera.position.z = 5;
 
 function animate() {
     renderer.render(scene, camera);
 
+    // Rotate the cube
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+    // Update the edges to match the cube's rotation
+    line.rotation.x = cube.rotation.x;
+    line.rotation.y = cube.rotation.y
 }
 renderer.setAnimationLoop(animate);
