402e6f25874feb1ec819656a27b2620ca602fc9c
Dory <pemegomachado@gmail.com>
2025-08-23 22:36:08 +0200

    Changed cube for a sphere

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Create a box
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.SphereGeometry(1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
