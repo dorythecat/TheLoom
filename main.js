ba98b6083e6df168b3f7bb96bc49058da1819d5c
Dory <pemegomachado@gmail.com>
2025-08-23 22:54:35 +0200

    Imrpoved rendering

--- main.js
+++ main.js
@@ -6,12 +6,15 @@ import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPa
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Create a sphere
-const geometry = new THREE.SphereGeometry(1);
+const geometry = new THREE.SphereGeometry(1, 64, 32);
 const material = new THREE.MeshBasicMaterial({ color: 0xaaaaaa });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
