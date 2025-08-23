eca90b2ff531397df592c06d16017eb8db5d7416
Dory <pemegomachado@gmail.com>
2025-08-23 23:31:49 +0200

    Added text (it spins :D)

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -27,6 +29,26 @@ const line = new THREE.LineSegments(
 );
 scene.add(line);
 
+// Load a font and create the text mesh
+const loader = new FontLoader();
+let textMesh;
+loader.load('https://unpkg.com/three@0.150.1/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+    const textGeometry = new TextGeometry('Spinning Text', {
+        font: font,
+        size: 0.4,
+        depth: 0.1,
+        curveSegments: 12
+    });
+    textGeometry.center();
+
+    const textMaterial = new THREE.MeshBasicMaterial({ color: 0xffaa00 });
+    textMesh = new THREE.Mesh(textGeometry, textMaterial);
+
+    // Position the text above the sphere
+    textMesh.position.y = 1.5;
+    scene.add(textMesh);
+});
+
 camera.position.z = 5;
 
 // Set up bloom composer
@@ -48,6 +70,8 @@ function animate() {
     // Update the edges to match the sphere's rotation
     line.rotation.x = cube.rotation.x;
     line.rotation.y = cube.rotation.y;
+
+    if (textMesh) textMesh.rotation.y += 0.02;
 
     composer.render(1 / 60);
 }
