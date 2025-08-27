d606e95e2ea05a3aa4deb50b9646c687cd80f5b2
dorythecat <pemegomachado@gmail.com>
2025-08-27 11:33:28 +0200

    Fixed rendering

--- main.js
+++ main.js
@@ -207,9 +207,9 @@ const maxNodes = 146;
 
 let nodes = []; // [{ index, position, name, text, baseScale, isNexus }]
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
 
-const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
